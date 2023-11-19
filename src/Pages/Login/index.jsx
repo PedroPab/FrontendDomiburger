da75df9405ad493f2cb76324b0bb8f3d864b04bf
Pedro Pablo <pmontesarroyave@gmail.com>
2023-11-19 16:02:06 -0500

    pusimos las lineas nesesaria para un mejor seo en el login y experimetar

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,15 +1,31 @@
 import { useContext } from 'react'
 import { MiContexto } from '../../Context'
 import { Container, Row, Card } from 'react-bootstrap'
+import { Helmet } from 'react-helmet';
 import NavbarRecepcion from "../../components/NavbarRecepcion";
 import Layout from "../../components/Layout";
 import FormLogin from '../../components/FormLogin';
+import imagenLogin from './../../assets/img/catLoginDomi.png';
+
+
 
 const Login = () => {
   const context = useContext(MiContexto)
 
   return (
     <>
+      <Helmet>
+        <title>Login</title>
+        <meta name="description" content="Ingresa tu usuario, contraseña y rol" />
+
+        {/* Open Graph / Facebook Meta Tags */}
+        {/* <meta property="og:url" content="https://tu-sitio-web.com/mi-pagina" /> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="App Domiburguer Login" />
+        <meta property="og:description" content="Ingresa tu usuario, contraseña y rol" />
+        <meta property="og:image" content={imagenLogin} />
+
+      </Helmet>
       <Layout>
 
         <NavbarRecepcion
