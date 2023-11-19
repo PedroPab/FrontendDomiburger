714b7d0878722ef858fcbd30dac32d2d8069b336
Pedro Pablo <pmontesarroyave@gmail.com>
2023-11-19 17:01:12 -0500

    puse un coso para motra que no hay pedidos

--- src/Pages/Cocina/index.jsx
+++ src/Pages/Cocina/index.jsx
@@ -1,13 +1,48 @@
 import { useContext } from 'react'
 import { MiContexto } from '../../Context'
-import NavbarCocinero from "../../components/NavbarCocinero";
+import NavbarCocinero from "../../components/NavbarDomiciliario copy";
 import Layout from "../../components/Layout";
 import { Container, Row } from 'react-bootstrap';
 import OrderCard from '../../components/OrderCard';
+import './index.css'
+import { FaBoxOpen } from 'react-icons/fa';
 
 
 const Cocina = () => {
   const context = useContext(MiContexto)
+
+  const ListPedidos = () => {
+
+    if (!context.items) return (<></>)
+
+    if (context.items.length <= 0) {
+      return (
+        <div className="sinPedidosContainer">
+          <div className="sinPedidosContent">
+            <FaBoxOpen size={50} /> {/* Tamaño del icono */}
+            <h2>Sin Pedidos</h2>
+          </div>
+        </div>
+      )
+    }
+
+    return (
+      context.items.map((pedido) => (
+        <div
+          className=' col-md-4 col-lg-4'
+          key={pedido.data.id}
+        >
+          <div
+            className="d-flex "
+          >
+            <OrderCard
+              dataPedido={pedido.data}
+            />
+          </div>
+        </div>
+      ))
+    )
+  }
 
   return (
     <>
@@ -20,25 +55,8 @@ const Cocina = () => {
         <Container fluid  >
 
           <Row className=''>
-            {
-              context.items ?
-                context.items.map((pedido) => (
-                  <div
-                    className=' col-md-4 col-lg-4'
-                    key={pedido.data.id}
-                  >
-                    <div
-                      className="d-flex "
-                    >
-                      <OrderCard
-                        dataPedido={pedido.data}
-                      />
-                    </div>
-                  </div>
-                )) :
-                <></>
-            }
 
+            <ListPedidos />
           </Row>
         </Container>
       </Layout >
