ef2363c97b16f28f89897f398ce74c54e6a2d23e
Pedro Pablo <pmontesarroyave@gmail.com>
2023-11-23 20:59:38 -0500

    creamos una pagina de 404

---
+++ src/Pages/NotFoundPage/index.jsx
@@ -0,0 +1,19 @@
+import { Container, Button } from 'react-bootstrap';
+
+const NotFoundPage = () => {
+  return (
+    <Container className="text-center mt-5">
+      <h1>¡Oops!</h1>
+      <h2>Parece que esta página se ha perdido en el espacio...</h2>
+      <img
+        src="https://example.com/your-404-image.png" // Reemplaza con la URL de tu imagen divertida
+        alt="Perdido en el espacio"
+        className="img-fluid my-4"
+      />
+      <p>Te recomendamos no esperar a que los extraterrestres te traigan de vuelta. ¡Mejor intenta con otra página!</p>
+      <Button href="/" variant="primary">Regresar a Casa</Button>
+    </Container>
+  );
+};
+
+export default NotFoundPage;
