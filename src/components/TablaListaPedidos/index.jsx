8600fd13e8b69da054fe4aa58c09f0bdf89f110f
Pedro Pablo <pmontesarroyave@gmail.com>
2023-11-24 23:15:33 -0500

    tenemo la tabla y las filas del resumen de los pedidos

---
+++ src/components/TablaListaPedidos/index.jsx
@@ -0,0 +1,36 @@
+import { Table } from 'react-bootstrap';
+import FilaPedidos from "../FilaPedido";
+
+
+const TablaListaPedidos = ({ pedidos }) => {
+  return (
+    <div className="table-responsive">
+      <Table striped bordered hover>
+        <thead>
+          {/* Encabezados de la tabla */}
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Hora</th>
+            <th scope="col">Estado</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Productos</th>
+            <th scope="col">Domiciliario</th>
+            <th scope="col">Total</th>
+            <th scope="col">Type</th>
+            <th scope="col">$?</th>
+            <th scope="col">Mas...</th>
+          </tr>
+        </thead>
+        <tbody>
+          <FilaPedidos
+            pedidos={pedidos}
+          />
+
+          {/* Iterar sobre pedidos para crear filas */}
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+
+export default TablaListaPedidos;
