0f009e4c1423c48ee3eff88929ab58e6217cdb57
Pedro Pablo <pmontesarroyave@gmail.com>
2024-03-02 19:32:14 -0500

    optimisamos la paginas de home de codigo y estadisticas

--- src/Pages/Codigo/index.jsx
+++ src/Pages/Codigo/index.jsx
@@ -1,23 +1,17 @@
-import { ContextProviderRecepcion } from '../../Context/RecepcionContex';
 import ListCardPages from '../../components/ListCardPages';
 import LayoutRecepcion from '../../Layout/Recepcion';
 
 const Codigos = () => {
-
   return (
     <>
       <LayoutRecepcion>
-        <ContextProviderRecepcion>
-
-          <ListCardPages
-            pages={[
-              { title: 'Codigo Referido', description: 'Crear codigo referido para un cliente', path: 'crearCodigoReferido' },
-              { title: 'Crear codigo ya existente', description: 'Importar o crear un codigo ya existente, para crear de una vez con los referidos y premios desde el principio', path: 'crearCodigoYaCreado' },
-              // { nombre: 'Clientes', path: 'clientes' },
-            ]}
-          />
-
-        </ContextProviderRecepcion>
+        <ListCardPages
+          pages={[
+            { title: 'Codigo Referido', description: 'Crear codigo referido para un cliente', path: 'crearCodigoReferido' },
+            { title: 'Crear codigo ya existente', description: 'Importar o crear un codigo ya existente, para crear de una vez con los referidos y premios desde el principio', path: 'crearCodigoYaCreado' },
+            // { nombre: 'Clientes', path: 'clientes' },
+          ]}
+        />
       </LayoutRecepcion >
     </>
   );
