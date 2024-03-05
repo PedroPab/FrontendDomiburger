39749cf48a742a701cb54a9c52e30d26cf58fc99
Pedro Pablo <pmontesarroyave@gmail.com>
2024-03-04 20:58:38 -0500

    pusimos en las variables las urls corectas para poder crear los codigos

--- src/Pages/Codigo/CrearCodigoReferido/index.jsx
+++ src/Pages/Codigo/CrearCodigoReferido/index.jsx
@@ -1,9 +1,8 @@
 import { useContext, } from 'react'
 import { MiContexto } from '../../../Context'
-import Layout from "../../../components/Layout";
 import { ContextProviderRecepcion } from '../../../Context/RecepcionContex';
 import { default as CrearCodigoReferidoComponent } from '../../../components/Codigos/CrearCodigoReferido';
-import { NavbarRecepcion } from '../../../components/Navbar/NavbarRecepcion';
+import LayoutRecepcion from '../../../Layout/Recepcion';
 
 const CrearCodigoReferido = () => {
 
@@ -13,13 +12,9 @@ const CrearCodigoReferido = () => {
 
   return (
     <>
-      <Layout>
+      <LayoutRecepcion>
         <ContextProviderRecepcion>
 
-          <NavbarRecepcion
-            modoOscuro={context.modoOscuro}
-            alternarModo={context.alternarModo}
-          />
 
           {/* crear codigo de referido */}
           <CrearCodigoReferidoComponent
@@ -31,7 +26,7 @@ const CrearCodigoReferido = () => {
 
         </ContextProviderRecepcion>
 
-      </Layout >
+      </LayoutRecepcion >
     </>
   );
 
--- src/Utils/api/codigos/createCodeReferidos.js
+++ src/Utils/api/codigos/createCodeReferidos.js
@@ -3,7 +3,7 @@ export const createCodeReferidos = async (data, token) => {
   // eslint-disable-next-line no-useless-catch
   try {
     const ENV = import.meta.env
-    const apiUrl = `${ENV.VITE_PROTOCOL_CODES}${ENV.VITE_HOST_CODES}:${ENV.VITE_PORT_CODES}`;
+    const apiUrl = `${ENV.VITE_PROTOCOL_CODES}${ENV.VITE_HOST_CODES}:${ENV.VITE_PORT_CODES}${ENV.VITE_CREATE_CODE || ''}`;
 
     const raw = JSON.stringify(data);
     const myHeaders = new Headers();
@@ -19,9 +19,10 @@ export const createCodeReferidos = async (data, token) => {
     const response = await fetch(`${apiUrl}/Referido`, requestOptions);
     const result = await response.json();
 
-    if (response.status !== 200 || response.status !== 201) {
+    if (response.status !== 200 && response.status !== 201) {
       throw result
     }
+
     return result
   } catch (error) {
     throw error
