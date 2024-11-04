ad181cd1faad554e92dc2d63641d7a37d5267ae1
Pedro Pablo <pmontesarroyave@gmail.com>
2024-11-04 01:12:41 -0500

    cambios para tener separados el server de socket io

--- src/Utils/socket.jsx
+++ src/Utils/socket.jsx
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 // eslint-disable-next-line react-refresh/only-export-components
 const ENV = import.meta.env
 
-const apiUrl = `${ENV.VITE_PROTOCOL}${ENV.VITE_HOST}:${ENV.VITE_PORT}`
+const apiUrl = `${ENV.VITE_HOST_WEB_SOCKET}`
 
 const socket = () => { return io(apiUrl) }; // Reemplaza con la URL de tu servidor Socket.IO
 
