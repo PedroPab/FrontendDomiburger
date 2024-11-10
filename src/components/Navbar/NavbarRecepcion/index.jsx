ca7ccdd1852d851dedcb493869472d37f6d8d3c3
Pedro Pablo <pmontesarroyave@gmail.com>
2024-11-10 00:59:50 -0500

    mejora el componente NavbarRecepcion al reorganizar el menú en secciones con dropdowns y agregar nuevos íconos para una mejor navegación

--- src/components/Navbar/NavbarRecepcion/index.jsx
+++ src/components/Navbar/NavbarRecepcion/index.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/prop-types */
-import { Container, Navbar, Nav, Button } from 'react-bootstrap';
-import { BsMoonStars } from 'react-icons/bs';
+import { Container, Navbar, Nav, NavDropdown, Button } from 'react-bootstrap';
+import { BsMoonStars, BsTools, BsFillPersonFill } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 import logo from './../../../assets/logo.png';
-import { BiAccessibility, BiArrowFromBottom, BiHappy, BiLogIn, BiMobile, BiTone } from "react-icons/bi";
-import { RecepcionContexto } from '../../../Context/RecepcionContex';
 import { useContext } from 'react';
-import { FaAngellist, FaMapMarkedAlt } from 'react-icons/fa';
+import { RecepcionContexto } from '../../../Context/RecepcionContex';
+import { FaMapMarkerAlt, FaRegChartBar, FaCashRegister } from 'react-icons/fa';
+import { MdOutlineDeliveryDining, MdOutlineAdminPanelSettings, MdOutlineSettings, MdOutlineReceipt } from "react-icons/md";
+import { FiMapPin, FiList } from "react-icons/fi";
 import { ReconnectButton } from '../ReconnectButton';
 import { ConnectionStatusIndicator } from '../ConnectionStatusIndicator';
 import { OrderCountIndicator } from '../OrderCountIndicator';
@@ -15,16 +16,15 @@ const NavbarRecepcion = ({ modoOscuro, alternarModo }) => {
   const contextRecepcion = useContext(RecepcionContexto);
 
   return (
-    <Navbar expand="lg" className='sticky-top' bg={modoOscuro ? 'dark' : 'light'}>
-      <Container>
+    <Navbar expand="lg" className="sticky-top" bg={modoOscuro ? 'dark' : 'light'}>
+      <Container fluid className="m-1">
         <Navbar.Brand as={Link} to="/">
           <img
             src={logo}  // Reemplaza con la ruta de tu logo
-            alt="Logo"
+            alt="Domiburguer"
             height="30"
             className="d-inline-block align-top"
           />
-          Domiburguer
         </Navbar.Brand>
 
         <ConnectionStatusIndicator />
@@ -33,19 +33,52 @@ const NavbarRecepcion = ({ modoOscuro, alternarModo }) => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="https://domiburguer.com/forAdmin" target="_blank"> <BiHappy /> Crear pedido</Nav.Link>
-            <Nav.Link as={Link} to="/formAdmin">  <BiTone /> Nuevo FormAdmin</Nav.Link>
-            <Nav.Link as={Link} to="/recepcion">  <FaAngellist /> Domi Rastreo</Nav.Link>
-            <Nav.Link as={Link} to="/mapRecepcion">  <FaMapMarkedAlt /> Map</Nav.Link>
-            <Nav.Link as={Link} to="/login">  <BiLogIn /> Login</Nav.Link>
-            <Nav.Link as={Link} to="/contabilidad">  <BiArrowFromBottom /> Contabilidad</Nav.Link>
-            <Nav.Link as={Link} to="/estadisticas"> Estadísticas</Nav.Link>
-            <Nav.Link as={Link} to="/codigos">  <BiAccessibility /> Codigos</Nav.Link>
-            <Nav.Link onClick={() => { contextRecepcion.openCloseModalAgregarDo() }}>  <BiMobile /> Domiciliarios</Nav.Link>
+            {/* 1. Sección de Crear Pedido */}
+            <NavDropdown title={<><MdOutlineReceipt className="me-2" />Crear Pedido</>} id="nav-dropdown-crear-pedido">
+              <NavDropdown.Item as={Link} to="/formAdmin">
+                <MdOutlineReceipt className="me-2" /> Crear pedido
+              </NavDropdown.Item>
+              <NavDropdown.Item href="https://domiburguer.com/forAdmin" target="_blank" className='text-muted'>
+                <FiList className="me-2" /> Antiguo
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            {/* 2. Sección de Seguimiento */}
+            <NavDropdown title={<><FiMapPin className="me-2" />Seguimiento</>} id="nav-dropdown-seguimiento">
+              <NavDropdown.Item as={Link} to="/recepcion">
+                <MdOutlineDeliveryDining className="me-2" /> Recepción
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/mapRecepcion">
+                <FaMapMarkerAlt className="me-2" /> Mapa
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            {/* 3. Sección de Administración */}
+            <NavDropdown title={<><MdOutlineAdminPanelSettings className="me-2" />Administración</>} id="nav-dropdown-administracion">
+              <NavDropdown.Item as={Link} to="/contabilidad">
+                <FaCashRegister className="me-2" /> Contabilidad
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/estadisticas">
+                <FaRegChartBar className="me-2" /> Estadísticas
+              </NavDropdown.Item>
+              <NavDropdown.Item onClick={() => contextRecepcion.openCloseModalAgregarDo()}>
+                <MdOutlineDeliveryDining className="me-2" /> Domiciliarios
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/codigos">
+                <FiList className="me-2" /> Códigos
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            {/* 4. Sección de Ajustes */}
+            <NavDropdown title={<><MdOutlineSettings className="me-2" />Ajustes</>} id="nav-dropdown-ajustes">
+              <NavDropdown.Item as={Link} to="/login">
+                <BsFillPersonFill className="me-2" /> Login
+              </NavDropdown.Item>
+              <NavDropdown.Item onClick={() => alternarModo()}>
+                <BsMoonStars className="me-2" /> Cambiar Tema
+              </NavDropdown.Item>
+            </NavDropdown>
           </Nav>
-          <Button variant={modoOscuro ? 'outline-light' : 'outline-dark'} onClick={() => alternarModo()}>
-            <BsMoonStars />
-          </Button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
