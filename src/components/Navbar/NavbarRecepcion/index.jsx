0ea74f3f9d56d9e2263108a7dd3e96c9bbf89290
Pedro Pablo <pmontesarroyave@gmail.com>
2024-11-10 01:18:58 -0500

    corregimos el bug del toogle que no asi nada

--- src/components/Navbar/NavbarRecepcion/index.jsx
+++ src/components/Navbar/NavbarRecepcion/index.jsx
@@ -40,64 +40,62 @@ const NavbarRecepcion = ({ modoOscuro, alternarModo }) => {
             <OrderCountIndicator />
           </Col>
 
-          {/* 4. Navbar Toggle and Collapse */}
-          <Col xs="auto" className="text-end">
+          {/* 4. Navbar Toggle */}
+          <Col xs="auto" className="text-end d-lg-none">
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
           </Col>
-
-          {/* 5. Menú de Navegación */}
-          <Col lg="auto" className="d-lg-block d-none">
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ms-auto">
-                {/* Sección de Crear Pedido */}
-                <NavDropdown title={<><MdOutlineReceipt className="me-2" />Crear Pedido</>} id="nav-dropdown-crear-pedido">
-                  <NavDropdown.Item as={Link} to="/formAdmin">
-                    <MdOutlineReceipt className="me-2" /> Crear pedido
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="https://domiburguer.com/forAdmin" target="_blank" className='text-muted'>
-                    <FiList className="me-2" /> Antiguo
-                  </NavDropdown.Item>
-                </NavDropdown>
-
-                {/* Sección de Seguimiento */}
-                <NavDropdown title={<><FiMapPin className="me-2" />Seguimiento</>} id="nav-dropdown-seguimiento">
-                  <NavDropdown.Item as={Link} to="/recepcion">
-                    <MdOutlineDeliveryDining className="me-2" /> Recepción
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/mapRecepcion">
-                    <FaMapMarkerAlt className="me-2" /> Mapa
-                  </NavDropdown.Item>
-                </NavDropdown>
-
-                {/* Sección de Administración */}
-                <NavDropdown title={<><MdOutlineAdminPanelSettings className="me-2" />Administración</>} id="nav-dropdown-administracion">
-                  <NavDropdown.Item as={Link} to="/contabilidad">
-                    <FaCashRegister className="me-2" /> Contabilidad
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/estadisticas">
-                    <FaRegChartBar className="me-2" /> Estadísticas
-                  </NavDropdown.Item>
-                  <NavDropdown.Item onClick={() => contextRecepcion.openCloseModalAgregarDo()}>
-                    <MdOutlineDeliveryDining className="me-2" /> Domiciliarios
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/codigos">
-                    <FiList className="me-2" /> Códigos
-                  </NavDropdown.Item>
-                </NavDropdown>
-
-                {/* Sección de Ajustes */}
-                <NavDropdown title={<><MdOutlineSettings className="me-2" />Ajustes</>} id="nav-dropdown-ajustes">
-                  <NavDropdown.Item as={Link} to="/login">
-                    <BsFillPersonFill className="me-2" /> Login
-                  </NavDropdown.Item>
-                  <NavDropdown.Item onClick={() => alternarModo()}>
-                    <BsMoonStars className="me-2" /> Cambiar Tema
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-            </Navbar.Collapse>
-          </Col>
         </Row>
+
+        {/* 5. Navbar Collapse (Menú de Navegación) */}
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            {/* Sección de Crear Pedido */}
+            <NavDropdown title={<><MdOutlineReceipt className="me-2" />Crear Pedido</>} id="nav-dropdown-crear-pedido">
+              <NavDropdown.Item as={Link} to="/formAdmin">
+                <MdOutlineReceipt className="me-2" /> Crear pedido
+              </NavDropdown.Item>
+              <NavDropdown.Item href="https://domiburguer.com/forAdmin" target="_blank" className='text-muted'>
+                <FiList className="me-2" /> Antiguo
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            {/* Sección de Seguimiento */}
+            <NavDropdown title={<><FiMapPin className="me-2" />Seguimiento</>} id="nav-dropdown-seguimiento">
+              <NavDropdown.Item as={Link} to="/recepcion">
+                <MdOutlineDeliveryDining className="me-2" /> Recepción
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/mapRecepcion">
+                <FaMapMarkerAlt className="me-2" /> Mapa
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            {/* Sección de Administración */}
+            <NavDropdown title={<><MdOutlineAdminPanelSettings className="me-2" />Administración</>} id="nav-dropdown-administracion">
+              <NavDropdown.Item as={Link} to="/contabilidad">
+                <FaCashRegister className="me-2" /> Contabilidad
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/estadisticas">
+                <FaRegChartBar className="me-2" /> Estadísticas
+              </NavDropdown.Item>
+              <NavDropdown.Item onClick={() => contextRecepcion.openCloseModalAgregarDo()}>
+                <MdOutlineDeliveryDining className="me-2" /> Domiciliarios
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/codigos">
+                <FiList className="me-2" /> Códigos
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            {/* Sección de Ajustes */}
+            <NavDropdown title={<><MdOutlineSettings className="me-2" />Ajustes</>} id="nav-dropdown-ajustes">
+              <NavDropdown.Item as={Link} to="/login">
+                <BsFillPersonFill className="me-2" /> Login
+              </NavDropdown.Item>
+              <NavDropdown.Item onClick={() => alternarModo()}>
+                <BsMoonStars className="me-2" /> Cambiar Tema
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
