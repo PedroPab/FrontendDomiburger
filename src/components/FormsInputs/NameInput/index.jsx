d5834a4230987cbf0000a64b86ad6e72420bf79f
Pedro Pablo <pmontesarroyave@gmail.com>
2024-12-23 14:38:42 -0500

    permite las tildes en el input de nombre

--- src/components/FormsInputs/NameInput/index.jsx
+++ src/components/FormsInputs/NameInput/index.jsx
@@ -4,7 +4,7 @@ import { FaUser } from 'react-icons/fa';
 const NameInput = ({ name, setName }) => {
   const handleChange = (e) => {
     //solo debe permitir letras y espacios y números
-    const rta = e.target.value.replace(/[^a-zA-Z0-9\s]/g, '')
+    const rta = e.target.value.replace(/[^a-zA-ZÀ-ÿ\u00f1\u00d1\s]/g, '')
     setName(rta);
   };
 
