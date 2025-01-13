5581f0a2cebe3d541072ca12bfaa56d319b00b31
Pedro Pablo <pmontesarroyave@gmail.com>
2025-01-13 01:36:34 -0500

    feat: integra contexto de Recepcion en Sidebar y reemplaza elementos por Domiciliarios

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,8 +1,11 @@
 import { Col } from 'react-bootstrap';
-import { useState } from 'react';
-import SidebarElement from './SidebarElement';
+import { useContext, useState } from 'react';
+import SidebarElementDelivery from './SidebarElement';
+import { RecepcionContexto } from '../../Context/RecepcionContex';
 
 const Sidebar = () => {
+  const contextRecepcion = useContext(RecepcionContexto)
+
   const [openSection, setOpenSection] = useState('dashboard');
 
   const handleToggle = (section) => {
@@ -12,35 +15,20 @@ const Sidebar = () => {
   return (
     <Col xs={3} md={2} className="vh-90 p-3 border-end">
       <ul className="list-unstyled">
-        <SidebarElement
-          handleToggle={handleToggle}
-          openSection={openSection}
-          title="Home"
-          eventKey="home"
-          imageUrl='https://i.pravatar.cc/150?img=3'
-        />
-        <SidebarElement
-          handleToggle={handleToggle}
-          openSection={openSection}
-          title="Dashboard"
-          eventKey="dashboard"
-          imageUrl="https://i.pravatar.cc/150?img=3"
-        />
-        <SidebarElement
-          handleToggle={handleToggle}
-          openSection={openSection}
-          title="Orders"
-          eventKey="orders"
-          imageUrl="https://i.pravatar.cc/150?img=3"
-        />
-        <li className="border-top my-3"></li>
-        <SidebarElement
-          handleToggle={handleToggle}
-          openSection={openSection}
-          title="Account"
-          eventKey="account"
-          imageUrl="https://i.pravatar.cc/150?img=3"
-        />
+
+        {
+          contextRecepcion.listDomiciliarios.map(domiciliario => (
+            <SidebarElementDelivery
+              key={domiciliario.id}
+              handleToggle={handleToggle}
+              openSection={openSection}
+              title={domiciliario.name}
+              eventKey={domiciliario.id}
+              imageUrl={`https://i.pravatar.cc/150?img=${domiciliario.id}`}
+            />
+          ))
+        }
+
       </ul>
     </Col>
   );
