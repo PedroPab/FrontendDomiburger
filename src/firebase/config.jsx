7ee0eb124fcbfbface6169285beb11dd157489f1
Pedro Pablo <pmontesarroyave@gmail.com>
2025-01-24 00:34:52 -0500

    feat: agrega consola de configuración de Firebase y actualiza la versión de Firebase en package.json

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -18,6 +18,7 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
+console.log(firebaseConfig)
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const FirebaseAuth = getAuth(app)
