40e1e414cc938daed95703a65b54b1abad7e49ea
Pedro Pablo <pmontesarroyave@gmail.com>
2025-02-01 13:12:36 -0500

    feat: mejora el componente RainbowBadge con un nuevo estilo especial y lógica aleatoria para mostrar badges únicos

--- src/components/CardProduct/RainbowBadge.jsx
+++ src/components/CardProduct/RainbowBadge.jsx
@@ -2,9 +2,15 @@ import { Badge } from 'react-bootstrap';
 import './RainbowBadge.css'; // Importamos los estilos
 
 const RainbowBadge = () => {
+  // Generar un número aleatorio y verificar si es menor a 1/500
+  const isSpecialBadge = Math.random() < 1 / 500;
+
+  // Seleccionar la clase según el resultado
+  const badgeClass = isSpecialBadge ? 'rainbow-badge-special' : 'rainbow-badge';
+
   return (
-    <Badge className="rainbow-badge position-absolute top-0 start-0 m-3 rounded-pill px-3 py-1 fs-6 shadow">
-      NUEVO
+    <Badge className={`${badgeClass} position-absolute top-0 start-0 m-3 rounded-pill px-3 py-1 fs-6 shadow`}>
+      {isSpecialBadge ? 'NUEVO!!!' : 'NUEVO'}
     </Badge>
   );
 };
