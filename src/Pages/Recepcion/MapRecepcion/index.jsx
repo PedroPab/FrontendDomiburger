8a7c4d8f6e0badcaf4651b42148021107db76d35
Pedro Pablo <pmontesarroyave@gmail.com>
2025-02-02 18:48:17 -0500

    feat: refactoriza el componente MapRecepcion para integrar el contexto de recepción y controlar la visibilidad del sidebar

--- src/Pages/Recepcion/MapRecepcion/index.jsx
+++ src/Pages/Recepcion/MapRecepcion/index.jsx
@@ -1,78 +1,77 @@
-import { useContext, useState } from 'react'
-import { MiContexto } from '../../../Context'
+import { useContext, useState } from 'react';
+import { MiContexto } from '../../../Context';
+import { RecepcionContexto } from '../../../Context/RecepcionContex';
 import { Col, Container, Row } from 'react-bootstrap';
-import { NavbarRecepcion } from "../../../components/Navbar/NavbarRecepcion";
-import Layout from "../../../components/Layout";
-import Mapa from "../../../components/MapsGoogle"
+import { NavbarRecepcion } from '../../../components/Navbar/NavbarRecepcion';
+import Layout from '../../../components/Layout';
+import Mapa from '../../../components/MapsGoogle';
 import { ContextProviderRecepcion } from '../../../Context/RecepcionContex';
 import SelectListDomiciliarios from '../../../components/SelectListDomiciliarios';
 import StickyCard from '../../../components/StickyCard';
-import OrderCard from '../../../components/OrderCard';
 import Sidebar from '../../../components/Sidebar';
 import ListOrder from './ListOrder';
 
-const MapRecepcion = () => {
-
-  const context = useContext(MiContexto)
+const MapRecepcionContent = () => {
+  // Uso de los contextos dentro del componente contenido, ya que aquí se encuentran envueltos por el Provider.
+  const context = useContext(MiContexto);
+  const recepcionContext = useContext(RecepcionContexto);
+  const showSidebar = recepcionContext.openSidebarFilterDelivery;
 
   const [centerMaps, setCenterMaps] = useState({
     lat: 6.29,
-    lng: -75.576
-  })
+    lng: -75.576,
+  });
 
   const containerStyle = {
-    width: '100%', // Establece el ancho al 100% del contenedor padre
-    height: '90vh', // Establece la altura al 100% de la altura de la ventana
+    width: '100%', // El mapa ocupará el 100% del ancho del contenedor padre
+    height: '90vh', // El mapa ocupará el 90% de la altura de la ventana
   };
 
   return (
     <>
-      <Layout>
-        <ContextProviderRecepcion>
-
-          <NavbarRecepcion
-            modoOscuro={context.modoOscuro}
-            alternarModo={context.alternarModo}
-          />
-          <Container fluid  >
-            <Row>
-              <Sidebar />
-              <Col xs={9} md={10}>
-                <Mapa
-                  zoom={context.zoomMaps}
-                  setZoomMaps={context.setZoomMaps}
-                  modoOscuro={context.modoOscuro}
-                  center={centerMaps}
-                  setCenter={setCenterMaps}
-                  containerStyle={containerStyle}
-                >
-                  <ListOrder
-                    items={context.items || []}
-                  />
-
-
-                </Mapa>
-              </Col>
-            </Row>
-          </Container>
-
-          {
-            context.idItemSelect &&
-            (<StickyCard
-              show={true}
-              pedidos={context?.items}
+      <NavbarRecepcion
+        modoOscuro={context.modoOscuro}
+        alternarModo={context.alternarModo}
+      />
+      <Container fluid>
+        <Row>
+          {/* Si showSidebar es true, se renderiza el Sidebar; de lo contrario, el contenido ocupará todo el ancho */}
+          {showSidebar && <Sidebar />}
+          <Col xs={showSidebar ? 9 : 12} md={showSidebar ? 10 : 12}>
+            <Mapa
+              zoom={context.zoomMaps}
+              setZoomMaps={context.setZoomMaps}
+              modoOscuro={context.modoOscuro}
+              center={centerMaps}
+              setCenter={setCenterMaps}
+              containerStyle={containerStyle}
             >
+              <ListOrder items={context.items || []} />
+            </Mapa>
+          </Col>
+        </Row>
+      </Container>
 
-            </StickyCard>)
-          }
+      {context.idItemSelect && (
+        <StickyCard show={true} pedidos={context.items}>
+          {/* Aquí puedes agregar contenido adicional al StickyCard si lo requieres */}
+        </StickyCard>
+      )}
 
-          <SelectListDomiciliarios />
-        </ContextProviderRecepcion>
-
-      </Layout >
+      <SelectListDomiciliarios />
     </>
   );
+};
 
-}
+const MapRecepcion = () => {
+  return (
+    <Layout>
+      {/* Se envuelve el contenido con el Provider correspondiente para que los hooks useContext funcionen correctamente */}
+      <ContextProviderRecepcion>
+        <MapRecepcionContent />
+      </ContextProviderRecepcion>
+    </Layout>
+  );
+};
 
-export default MapRecepcion
+export default MapRecepcion;
