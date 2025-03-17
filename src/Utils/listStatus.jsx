1108674b2eea1eb435a095b7a68b32b778e82e57
PedroPab <pmontesarroyave@gmail.com>
2025-03-16 19:05:47 -0500

    feat: enhance order status management with next status button and update status labels

--- src/Utils/listStatus.jsx
+++ src/Utils/listStatus.jsx
@@ -4,91 +4,100 @@ import { ORDER_STATUSES } from "./const/status"
 const statusOrderCol = {}
 
 statusOrderCol[ORDER_STATUSES.PENDING_CONFIRMATION] = {
-    color: '#FFC107', // warning - amber
-    label: 'Pendiente de confirmación',
-    name: ORDER_STATUSES.PENDING_CONFIRMATION,
-    icon: <FaExclamationCircle />
+	color: '#FFC107', // warning - amber
+	label: 'Pendiente de confirmación',
+	textNextStatus: 'A Recientes',
+	name: ORDER_STATUSES.PENDING_CONFIRMATION,
+	icon: <FaExclamationCircle />
 }
 
 statusOrderCol[ORDER_STATUSES.FRESH] = {
-    color: '#17A2B8', // info - teal
-    label: 'Recién creado',
-    name: ORDER_STATUSES.FRESH,
-    icon: <FaExclamationCircle />
+	color: '#17A2B8', // info - teal
+	label: 'Recién creado',
+	textNextStatus: 'Preparar',
+	name: ORDER_STATUSES.FRESH,
+	icon: <FaExclamationCircle />
 }
 
 statusOrderCol[ORDER_STATUSES.CONFIRMED] = {
-    color: '#28A745', // success - green
-    label: 'Confirmado',
-    name: ORDER_STATUSES.CONFIRMED,
-    icon: <FaCheckCircle />
+	color: '#28A745', // success - green
+	label: 'Confirmado',
+	textNextStatus: 'En espera',
+	name: ORDER_STATUSES.CONFIRMED,
+	icon: <FaCheckCircle />
 }
 
 statusOrderCol[ORDER_STATUSES.ON_HOLD] = {
-    color: '#6C757D', // secondary - gray
-    label: 'En espera',
-    name: ORDER_STATUSES.ON_HOLD,
-    icon: <FaClock />
+	color: '#6C757D', // secondary - gray
+	label: 'En espera',
+	textNextStatus: 'A espera',
+	name: ORDER_STATUSES.ON_HOLD,
+	icon: <FaClock />
 }
 
 statusOrderCol[ORDER_STATUSES.PREPARING] = {
-    color: '#007BFF', // primary - blue
-    label: 'Preparando',
-    name: ORDER_STATUSES.PREPARING,
-    icon: <FaBoxOpen />
+	color: '#007BFF', // primary - blue
+	label: 'Preparando',
+	textNextStatus: 'Creado',
+	name: ORDER_STATUSES.PREPARING,
+	icon: <FaBoxOpen />
 }
 
 statusOrderCol[ORDER_STATUSES.READY_FOR_PICKUP] = {
-    color: '#17A2B8', // info - teal
-    label: 'Listo para recoger',
-    name: ORDER_STATUSES.READY_FOR_PICKUP,
-    icon: <FaClipboardCheck />
+	color: '#17A2B8', // info - teal
+	label: 'Listo para recoger',
+	textNextStatus: 'Despachar',
+	name: ORDER_STATUSES.READY_FOR_PICKUP,
+	icon: <FaClipboardCheck />
 }
 
 statusOrderCol[ORDER_STATUSES.DISPATCHED] = {
-    color: '#FFC107', // warning - amber
-    label: 'Despachado',
-    name: ORDER_STATUSES.DISPATCHED,
-    icon: <FaTruck />
+	color: '#FFC107', // warning - amber
+	label: 'Despachado',
+	textNextStatus: 'Entregado',
+	name: ORDER_STATUSES.DISPATCHED,
+	icon: <FaTruck />
 }
 
 statusOrderCol[ORDER_STATUSES.DELIVERED] = {
-    color: '#28A745', // success - green
-    label: 'Entregado',
-    name: ORDER_STATUSES.DELIVERED,
-    icon: <FaCheckCircle />
+	color: '#28A745', // success - green
+	label: 'Entregado',
+	textNextStatus: 'Facturar',
+	name: ORDER_STATUSES.DELIVERED,
+	icon: <FaCheckCircle />
 }
 
 statusOrderCol[ORDER_STATUSES.PENDING_TRANSFER] = {
-    color: '#FFC107', // warning - amber
-    label: 'Pendiente de transferencia',
-    name: ORDER_STATUSES.PENDING_TRANSFER,
-    icon: <FaExchangeAlt />
+	color: '#FFC107', // warning - amber
+	label: 'Pendiente de transferencia',
+	textNextStatus: 'Facturar',
+	name: ORDER_STATUSES.PENDING_TRANSFER,
+	icon: <FaExchangeAlt />
 }
 
 statusOrderCol[ORDER_STATUSES.CODE_ERROR] = {
-    color: '#DC3545', // danger - red
-    label: 'Error de código',
-    name: ORDER_STATUSES.CODE_ERROR,
-    icon: <FaBug />
+	color: '#DC3545', // danger - red
+	label: 'Error de código',
+	name: ORDER_STATUSES.CODE_ERROR,
+	icon: <FaBug />
 }
 
 const listStatusRecepcion = [
-    statusOrderCol[ORDER_STATUSES.PENDING_CONFIRMATION],
-    statusOrderCol[ORDER_STATUSES.FRESH],
-    statusOrderCol[ORDER_STATUSES.CONFIRMED],
-    statusOrderCol[ORDER_STATUSES.ON_HOLD],
-    statusOrderCol[ORDER_STATUSES.PREPARING],
-    statusOrderCol[ORDER_STATUSES.READY_FOR_PICKUP],
-    statusOrderCol[ORDER_STATUSES.DISPATCHED],
-    statusOrderCol[ORDER_STATUSES.DELIVERED],
-    statusOrderCol[ORDER_STATUSES.PENDING_TRANSFER],
-    statusOrderCol[ORDER_STATUSES.CODE_ERROR]
+	statusOrderCol[ORDER_STATUSES.PENDING_CONFIRMATION],
+	statusOrderCol[ORDER_STATUSES.FRESH],
+	statusOrderCol[ORDER_STATUSES.CONFIRMED],
+	statusOrderCol[ORDER_STATUSES.ON_HOLD],
+	statusOrderCol[ORDER_STATUSES.PREPARING],
+	statusOrderCol[ORDER_STATUSES.READY_FOR_PICKUP],
+	statusOrderCol[ORDER_STATUSES.DISPATCHED],
+	statusOrderCol[ORDER_STATUSES.DELIVERED],
+	statusOrderCol[ORDER_STATUSES.PENDING_TRANSFER],
+	statusOrderCol[ORDER_STATUSES.CODE_ERROR]
 ]
 
 export {
-    statusOrderCol,
-    listStatusRecepcion
+	statusOrderCol,
+	listStatusRecepcion
 }
 
 // [
--- src/components/OrderCardV2/CardFooter.jsx
+++ src/components/OrderCardV2/CardFooter.jsx
@@ -1,58 +1,21 @@
-import { Card, Button, Spinner } from "react-bootstrap";
-import { useAuth } from "../../Context/AuthContext";
-import { OrderService } from "../../apis/clientV2/OrderService";
-import { toast } from "react-toastify";
-import { useState } from "react";
-import { statusNext } from "../../Utils/const/statusChange/listNextStatus";
-import { statusOrderCol } from "../../Utils/listStatus";
-import { DeliveryDropdown } from "./DeliveryDropdown";
+import { Card } from "react-bootstrap";
 import { usePreferences } from "../../Context/PreferencesContext";
 import { ROLES } from "../../Utils/const/roles";
+import { DeliveryDropdown } from "./DeliveryDropdown";
+import { NextStatusButton } from "./NextStatusButton";
 
 function CardFooterComponent({ data }) {
 	const { roleSelect } = usePreferences();
 
-	const { token } = useAuth();
-	const orderService = new OrderService(token);
-
-	const [loadChangeStatus, setLoadChangeStatus] = useState(false);
-	const nextStatus = () => {
-		const updateOrderStatus = async () => {
-			const id = data.id;
-			const previousState = data.status;
-			const nextState = statusNext(previousState);
-			try {
-				await orderService.changeStatus(id, previousState, nextState);
-
-				setLoadChangeStatus(false);
-				toast.success("Estado de la orden actualizado");
-			} catch (error) {
-				console.log("🚀 ~ updateOrderStatus ~ error:", error)
-				setLoadChangeStatus(false);
-				toast.error(`Error al cambiar el estado de la orden: ${error?.message}`);
-			}
-		};
-		setLoadChangeStatus(true);
-		updateOrderStatus();
-	};
-
-	//consultamos lo datos del 
-
-	const nameStatus = statusOrderCol[data.status]?.label
-
 	return (
-		<Card.Footer className="d-flex justify-content-between align-items-center">
-
-			{roleSelect == ROLES.ADMIN.value || roleSelect == ROLES.RECEPTION.value && (
+		<Card.Footer className="d-flex flex-wrap justify-content align-items-center gap-2">
+			{(roleSelect === ROLES.ADMIN.value || roleSelect === ROLES.RECEPTION.value) && (
 				<DeliveryDropdown
 					assignedCourierUserId={data?.assignedCourierUserId}
 					orderId={data.id}
 				/>
 			)}
-
-			<Button variant="primary" onClick={nextStatus} disabled={loadChangeStatus}>
-				{loadChangeStatus ? <Spinner animation="border" size="sm" /> : nameStatus || "Cambiar estado"}
-			</Button>
+			<NextStatusButton data={data} />
 		</Card.Footer>
 	);
 }
