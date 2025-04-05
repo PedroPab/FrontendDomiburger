8868e46cd5d36de79adcc570b9daeca772519357
PedroPab <pmontesarroyave@gmail.com>
2025-04-05 17:27:24 -0500

    fix: enhance DeliveryRow component to conditionally render delivery details or alert message

--- src/components/ResumenProductosForm/DeliveryRow.jsx
+++ src/components/ResumenProductosForm/DeliveryRow.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
 import { DurationSpan } from '../common/delivery/DurationSpan';
 import { DistanceSpan } from '../common/delivery/DistanceSpan';
 
 const DeliveryRow = ({ delivery }) => {
-	if (!(delivery && delivery.distance && delivery.duration && delivery.price)) {
-		return null;
-	}
 
 	return (
 		<tr>
 			<th>Domicilio</th>
 			<th>
 				<div className="d-flex align-items-center">
-					<DurationSpan duration={delivery.duration + (15 * 60)} />
-					<span className="mx-3"></span>
-					<DistanceSpan distance={delivery.distance} />
+					{
+						delivery && delivery?.distance && delivery?.duration && delivery?.price ?
+							(
+								<>
+									<DurationSpan duration={delivery.duration + (15 * 60)} />
+									<span className="mx-3"></span>
+									<DistanceSpan distance={delivery.distance} />
+								</>
+							) :
+							(
+								<div className="text-danger" role="alert">
+									Sin domicilio !!!
+								</div>
+							)
+					}
+
 				</div>
 			</th>
 			<th>
-				<span>${delivery.price.toLocaleString()}</span>
+				<span>${(delivery?.price || 0).toLocaleString()}</span>
 			</th>
 		</tr>
 	);
