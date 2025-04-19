487d7f333880ff7b7f9ad24aaeaa0abf7dc6cc23
PedroPab <pmontesarroyave@gmail.com>
2025-04-19 14:13:41 -0500

    feat: move CardProductResumid component to its own file for better organization and maintainability

--- src/components/AdditionSelector/index.jsx
+++ src/components/AdditionSelector/index.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
-import { Button, Modal, Card, Row, Col } from 'react-bootstrap';
+import { Button, Modal, Row, Col } from 'react-bootstrap';
+import { CardProductResumid } from '../Products/CardProductResumid';
 
 const AdditionSelector = ({ producto, adiciones, onChangeSelect }) => {
 	const [show, setShow] = useState(false);
@@ -65,40 +66,5 @@ const AdditionSelector = ({ producto, adiciones, onChangeSelect }) => {
 	);
 };
 
-const CardProductResumid = ({ product, onClick }) => (
-	<button
-		// type="button"
-		className="card p-0 h-100 w-100"
-		onClick={() => onClick(product)}
-		onKeyPress={(e) => {
-			if (e.key === 'Enter' || e.key === ' ') onClick(product);
-		}}
-		style={{ cursor: 'pointer', border: 'none', background: 'none' }}
-	>
-		<Card
-		>
-			{/* {product.photos?.[0] && (
-				<Card.Img
-					variant="top"
-					src={product.photos[0]}
-					style={{ height: 120, objectFit: 'cover' }}
-				/>
-			)} */}
-			<Card.Body>
-				<Card.Title>{product.name}</Card.Title>
-				<Card.Text className="mb-1 text-truncate">{product.description}</Card.Text>
-				<Card.Text className="fw-bold">${product.price.toLocaleString()}</Card.Text>
-				<div
-					style={{
-						backgroundColor: product.colorPrimary,
-						width: '100%',
-						height: '6px',
-						borderRadius: '3px',
-					}}
-				/>
-			</Card.Body>
-		</Card>
-	</button>
-);
 
 export { AdditionSelector };
